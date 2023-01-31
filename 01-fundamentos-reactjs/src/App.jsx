6844d35aa18d410c8d7dd0804d201eeccc08dbb6
Matheus Adriano Martins <halkner77@hotmail.com>
2023-01-31 17:53:39 -0300

    Header component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,21 @@
+import { Header } from "./components/Header/Header"
 import { Post } from "./Post"
+import './global.css'
 
 export function App() {
   return (
     <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Header />
+      
+        <Post 
+          author="Matheus Adriano Martins"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, nihil dignissimos! Hic libero perferendis rem tempora asperiores vero odit."
+        />
+
+        <Post 
+          author="Cristiana Mariano Elias"
+          content="Um novo post legal"
+        />
     </div>
 
   )
